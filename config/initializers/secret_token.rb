1118c9fde3fbbf05edaa11b0682d5d023620a5eb
sara-g <platinummix@yahoo.com>
2013-04-24 22:59:03 -0500

    pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tryzee::Application.config.secret_token = 'dce58aaba4f9c9fcdd1b5b1e8f4aef78b7728e90901998b772699098a5f8f33b82cd4d89660d30f5b9fe07c690d57c8a6bc3abee59e35facfc1615edeacfe3e6'
